0eeed80606cce66fb3ff4943460744a04f6868c0
Teja Reddy <narasimteja@gmail.com>
2025-10-25 11:48:05 -0400

    Initial Commit

---
+++ mesh=-pay/mesh__payApp.swift
@@ -0,0 +1,17 @@
+//
+//  mesh__payApp.swift
+//  mesh=-pay
+//
+//  Created by Narasimha Teja Reddy on 10/25/25.
+//
+
+import SwiftUI
+
+@main
+struct mesh__payApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
